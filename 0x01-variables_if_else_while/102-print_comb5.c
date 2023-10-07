8d29217e5dc6b7b391dd223161943c743c55a488
Williams Michael <willmich067@gmail.com>
2023-10-06 23:33:59 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a, b, c, d;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			for (c = a; c <= 9; c++)
+			{
+				for (d = 0; d <= 9; d++)
+				{
+					if (a != c || b < d)
+					{
+						putchar(a + '0');
+						putchar(b + '0');
+						putchar(' ');
+						putchar(c + '0');
+						putchar(d + '0');
+					if (a != 9 || b != 8 || c != 9 || d != 9)
+						{
+							putchar(',');
+							putchar(' ');
+						}
+					}
+				}	
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
