c05623da0de56f3641f3420517d29d5a690f927a
Williams Michael <willmich067@gmail.com>
2023-10-12 19:14:57 -0700

    3-main_b.c

--- 0x03-debugging/3-main_b.c
+++ 0x03-debugging/3-main_b.c
@@ -16,8 +16,12 @@ int main(void)
 	month = 4;
 	day = 01;
 	year = 1997;
+
 	printf("Date: %02d/%02d/%04d\n", month, day, year);
+
 	day = convert_day(month, day);
+
 	print_remaining_days(month, day, year);
+
 	return (0);
 }
