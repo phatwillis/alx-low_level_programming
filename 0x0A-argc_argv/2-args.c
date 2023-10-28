1714185ca3f3d24e72fd977e5f5f8bfc071f9516
Williams Michael <willmich067@gmail.com>
2023-10-27 18:51:37 -0700

    updated

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,46 +1,20 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
 /**
- * main - function
- * @argc: length of argv
- * @argv: number of argument
- * Return: Always 0
+ * main - Entry point of the program
+ * @argc: The number of command-line arguments, including the program name
+ * @argv: An array of strings containing the command-line arguments
+ *
+ * Return: 0 (Always, success)
  */
 int main(int argc, char *argv[])
 {
-	/*Declaring variables*/
-	int position, total, change, aux;
-	int coins[] = {25, 10, 5, 2, 1}; /*Array int*/
+	int i;
 
-	position = total = change = aux = 0;
-
-	if (argc != 2)
+	for (i = 0; i < argc; i++)
 	{
-		printf("Error\n");
-		return (1);
+		printf("%s\n", argv[i]);
 	}
-
-	total = atoi(argv[1]); /*Covert str to int*/
-
-	if (total <= 0)
-	{
-		printf("0\n");
-		return (0);
-	}
-
-	/*Declaring While*/
-	while (coins[position] != '\0')
-	{
-		if (total >= coins[position])
-		{
-			aux = (total / coins[position]);
-			change += aux;
-			total -= coins[position] * aux;
-		}
-
-		position++;
-	}
-	printf("%d\n", change);
-
 	return (0);
 }
+
