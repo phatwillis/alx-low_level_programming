808f5f270cb1749d1df693d49af680e29ee7f926
Williams Michael <willmich067@gmail.com>
2023-11-02 19:56:57 -0700

    0x0c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,89 +1,152 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
+#include <stdlib.h>
 
-/*
- * Check if a string is composed of digits
- */
-int is_digit(char *str)
+/**
+ * _atoi_digit - convert a char to integer.
+ * @x: character to convert.
+ * Return: integer.
+ **/
+
+int _atoi_digit(char x)
 {
-	while (*str)
-	{
-		if (*str < '0' || *str > '9')
-		{
-			return (0);
-		}
-		str++;
-	}
-	return (1);
+	unsigned int res;
+
+	if (x <= '9' && x >= '0')
+		res = x - '0';
+	return (res);
 }
 
-/*
- * Multiply two positive numbers
- */
-void multiply(char *num1, char *num2)
+/**
+ * _isNumber - Define if a string is a number.
+ * @argv: Pointer to string.
+ * Return: success (0).
+ **/
+int _isNumber(char *argv)
 {
-	char *result;
-	int len1 = strlen(num1);
-	int len2 = strlen(num2);
-	int len_result = len1 + len2;
-	int carry, i, j, k;
+	int i;
 
-	result = (char *)malloc(len_result + 1);
-
-	for (i = 0; i <= len_result; i++)
-	{
-		result[i] = '0';
-	}
-
-	for (i = len1 - 1; i >= 0; i--)
-	{
-		carry = 0;
-		for (j = len2 - 1, k = i + len2; j >= 0; j--, k--)
-		{
-			int temp = (num1[i] - '0') * (num2[j] - '0') + (result[k] - '0') + carry;
-
-			carry = temp / 10;
-			result[k] = (temp % 10) + '0';
-		}
-		result[k] = (result[k] - '0') + carry + '0';
-	}
-
-	/*
-	 * Remove leading zeros
-	 */
-	for
-		(i = 0; result[i] == '0'; i++);
-
-	if (result[i] == '\0')
-	{
-		printf("0\n");
-	} else
-	{
-		printf("%s\n", result + i)
-	}
-
-	free(result);
+	for (i = 0; argv[i]; i++)
+		if (argv[i] < 48 || argv[i] > 57)
+			return (1);
+	return (0);
 }
 
-/*
- * Command-line argument validation and program logic
+/**
+ *_calloc - allocate array of size * nmemb.
+ * @nmemb: number of elements.
+ * @size: size of element.
+ * Return: pointer to array.
+ **/
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *tab;
+	unsigned int i;
+
+	tab = malloc(size * nmemb);
+
+	if (tab == NULL)
+		return (NULL);
+
+	for (i = 0; i < (size * nmemb); i++)
+		tab[i] = '0';
+
+	return (tab);
+}
+
+/**
+ * mul_array - multiply two arrays.
+ * @a1: first array.
+ * @len1: length of array a1.
+ * @a2:  char.
+ * @a3: array for result.
+ * @lena: length of array a3.
+ * Return: pointer to array.
+ **/
+
+void *mul_array(char *a1, int len1, char a2, char *a3, int lena)
+{
+	int mul = 0, i, k;
+
+	k = lena;
+	for (i = len1 - 1; i >= 0 ; i--)
+	{
+		mul += (a1[i] - '0') * (a2 - '0') + (a3[k] - '0');
+		a3[k] = (mul % 10) + '0';
+		mul /= 10;
+		k--;
+	}
+
+		while (mul != 0)
+		{
+			mul += a3[k] - '0';
+			a3[k] = (mul % 10) + '0';
+			mul /= 10;
+			k--;
+		}
+
+	return (a3);
+}
+/**
+ * print_array - print all digits of array.
+ * @nb: number of elements to print.
+ * @a: array of elements.
+ **/
+void print_array(char *a, int nb)
+{
+	int i = 0;
+
+	while (a[i] == '0' && (i + 1) < nb)
+	{
+		i++;
+	}
+	for (; i < nb; i++)
+	{
+		_putchar(a[i]);
+	}
+	_putchar('\n');
+}
+
+/**
+ *main - print the multiplication of 2 numbers.
+ *@argc: array length.
+ *@argv: array.
+ *Return: 0.
  */
+
 int main(int argc, char *argv[])
 {
-	if (argc != 3)
+	int i, c, len1, len2, lenres;
+	char E[6] = {'E', 'r', 'r', 'o', 'r', '\n'};
+	char *tabres;
+
+	if (argc != 3 || _isNumber(argv[1]) == 1 || _isNumber(argv[2]) == 1)
 	{
-		printf("Error\n");
-		return (98);
+		for (i = 0; i < 6; i++)
+		{
+			_putchar(E[i]);
+		}
+		exit(98);
 	}
-
-	if (!is_digit(argv[1]) || !is_digit(argv[2]))
+	for (len1 = 0; argv[1][len1]; len1++)
+	;
+	for (len2 = 0; argv[2][len2]; len2++)
+	;
+	lenres = len1 + len2;
+	tabres = _calloc(lenres, sizeof(int));
+	if (tabres == NULL)
 	{
-		printf("Error\n");
-		return (98);
+		free(tabres);
+		return (0);
 	}
-
-	multiply(argv[1], argv[2);
-
+	for (i = len2 - 1, c = 0; i >= 0; i--)
+	{
+	tabres = mul_array(argv[1], len1, argv[2][i], tabres, (lenres - 1 - c));
+	c++;
+	}
+	print_array(tabres, lenres);
+	free(tabres);
+	exit(EXIT_SUCCESS);
 	return (0);
 }
