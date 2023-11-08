fc5ac426ff09573a247946d3f7abebc98f388410
Williams Michael <willmich067@gmail.com>
2023-11-08 15:29:29 -0800

    0x0F

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+
+void print_opcodes(int num_bytes);
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	int num_bytes = atoi(argv[1]);
+
+	if (num_bytes < 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
+
+	print_opcodes(num_bytes);
+
+	return (0);
+}
+
+void print_opcodes(int num_bytes)
+{
+	unsigned char *main_ptr = (unsigned char *)main;
+
+	for (int i = 0; i < num_bytes; ++i)
+	{
+		printf("%02x ", main_ptr[i]);
+	}
+
+	printf("\n");
+}
